2d8a7834f336d386206acbcd0cc8ef83e220ce1e
Anand, Gurvin Singh <gurvin.anand@gmail.com>
2025-05-27 18:04:03 -0400

    starting

---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, RequestHandler } from 'express'
+import cors from 'cors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: { origin: '*' }
+})
+
+app.use(cors())
+app.use(express.json())
+
+io.on('connection', socket => {
+  console.log('a user connected:', socket.id)
+
+  socket.on('move', data => {
+    socket.broadcast.emit('move', data)
+  })
+})
+
+const homeHandler: RequestHandler = (_req, res) => {
+  res.send('Chaotic Smash Server Running')
+}
+app.get('/', homeHandler)
+
+httpServer.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000')
+})
